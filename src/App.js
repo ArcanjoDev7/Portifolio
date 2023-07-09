c3cee7ba3d06921130c9206bd6dcd50e9a2ff41a
blxsty <miguelarcanjodearaujocosta@gmail.com>
2023-07-08 23:46:14 -0300

    sobre/conhecimentos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ola mundo </h1>
-      <p>salve salve</p>
-    </div>
-  );
-}
+function Portfolio() {
+    const technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
 
-export default App;
+    return (
+        <div className="container">
+          <h1 className="heading">Sobre Mim</h1>
+          <p className="description">
+            Olá! Meu nome é Miguel Arcanjo e sou um desenvolvedor full-stack com tecnologias da mobile. Adoro criar soluções criativas e inovadoras, e estou constantemente aprendendo e me atualizando com as últimas tendências da indústria.
+          </p>
+          <div>
+          <h2>Conhecimentos</h2>
+      <ul className="technology-list">
+        {technologies.map((technology, index) => (
+          <li key={index}>{technology}</li>
+        ))}
+      </ul>
+    </div>
+        </div>
+        
+      );
+    }
+export default Portfolio;
